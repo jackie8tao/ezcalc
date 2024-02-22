4cb8a920dd6150222ed6979bb78347c6ae668a5f
jackietao <jackie8tao@outlook.com>
2024-02-22 19:52:26 +0800

    feat: initialize project

---
+++ easycalc.go
@@ -0,0 +1,17 @@
+package easycalc
+
+// ScriptLang dsl分析模块
+type ScriptLang interface {
+}
+
+// Sinker 数据存储模块
+type Sinker interface {
+}
+
+// Connector 连接器模块，用于获取数据
+type Connector interface {
+}
+
+// Engine 计算引擎
+type Engine interface {
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module easycalc
+
+go 1.21
